40d27a390e390c7aee0f090d98803de0f093bf6e
Joseph Kamandy <josephkamandy@gmail.com>
2024-02-25 12:45:55 -0800

    added filter feature for changing difficulty level on exerciseList.js

--- MFF-Frontend/Screens/Exercise/ExerciseList.js
+++ MFF-Frontend/Screens/Exercise/ExerciseList.js
@@ -1,20 +1,107 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import { SafeAreaView,ScrollView, View, TextInput, Text, StyleSheet, Pressable, Animated } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+// import SelectDropdown from 'react-native-select-dropdown'
+import levenshtein from 'fast-levenshtein'
+import { SelectList } from 'react-native-dropdown-select-list'
+import RNPickerSelect from 'react-native-picker-select';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+
+const muscleTypes = [
+  { label: "Abdominals", value: 'abdominals' },
+  { label: "Abductors", value: "abductors" },
+  { label: "Adductors", value: 'adductors' },
+  { label: 'Biceps', value: "biceps" },
+  { label: 'Calves', value: "calves" },
+  { label: 'Chest', value: "chest" },
+  { label: 'Forearms', value: "forearms" },
+  { label: 'Glutes', value: "glutes" },
+  { label: 'Hamstrings', value: "hamstrings" },
+  { label: 'Lats', value: "lats" },
+  { label: 'Lower Back', value: "lower_back" },
+  { label: 'Middle Back', value: "middle_back" },
+  { label: 'Neck', value: "neck" },
+  { label: 'Quadriceps', value: "quadriceps" },
+  { label: 'Traps', value: "traps" },
+  { label: 'Triceps', value: "triceps" },
+];
+
+const exerciseTypes = [
+  { label: 'Cardio', value: 'cardio' },
+  { label: 'Olympic Weightlifting', value: 'olympic_weightlifting' },
+  { label: 'Plyometrics', value: 'plyometrics' },
+  { label: 'Powerlifting', value: 'powerlifting' },
+  { label: 'Strength', value: 'strength' },
+  { label: 'Stretching', value: 'stretching' },
+  { label: 'Strongman', value: 'strongman' },
+];
+
+const difficultyTypes = [
+  { label: 'Beginner', value: 'beginner' },
+  { label: 'Intermediate', value: 'intermediate' },
+  { label: 'Expert', value: 'expert' },
+];
+
 
 const ExerciseList = ({ navigation }) => {
     const [muscle, setMuscle] = useState('');
+    const [closestMuscle, setClosestMuscle] = useState('')
     const [exercises, setExercises] = useState([]);
+    const [filteredExercises, setFilteredExercises] = useState([])
+
+    const [muscleType, setMuscleType] = useState('');
+    const [exerciseType, setExerciseType] = useState('');
+    const [difficultyType, setDifficultyType] = useState('');
+  
+    // useEffect(() => {
+    //   // Filter the data based on the selected filters
+    //   const filteredResult = exercises.filter((item) => {
+    //     const exerciseTypeMatch = !exerciseType || item.type === exerciseType;
+    //     const muscleTypeMatch = !muscleType || item.muscle === muscleType;
+    //     const difficultyTypeMatch = !difficultyType || item.difficulty === difficultyType;
+    
+    //     return exerciseTypeMatch && muscleTypeMatch && difficultyTypeMatch;
+    //   });
+    
+    //   // Update the filtered data state
+    //   setExercises(filteredResult);
+    // }, [exercises, exerciseType, muscleType, difficultyType]);
+
 
     const viewPlan = () => {
         navigation.navigate('WorkoutPlan'); 
       };
+
+    findClosestLevenshtein = () => { 
+      let minDistance = Infinity;
+
+      muscleTypes.forEach((option) => {
+        const distance = levenshtein.get(muscle, option.label)
+        
+        if (distance < minDistance) {
+          console.log('muscle: ' + muscle)
+          console.log('option.label: ' + option.label)
+
+          minDistance = distance
+          setClosestMuscle(option.value)
+        }
+      })
+      console.log(closestMuscle)
+    }
+
+    handleSetMuscle = (value) => {
+      // console.log('handle muscle: ' + value)
+      setMuscle(value)
+      findClosestLevenshtein()
+    }
     
     const handleSearch = async () => {
       try {
         const response = await fetch(
-          'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
+          'https://api.api-ninjas.com/v1/exercises?muscle=' + closestMuscle,
           {
             method: 'GET',
             headers: {
@@ -24,42 +111,122 @@ const ExerciseList = ({ navigation }) => {
         );
         const data = await response.json();
         setExercises(data); 
+        setFilteredExercises(data);
       } catch (error) {
         console.error(error);
       }
     };
+
+
+
   return (
     <SafeAreaView style={styles.safeArea}>
         
       <View style={styles.container}>
+        {/* <Text style={styles.titleText}>
+          Type in a muscle group to get started
+        </Text> */}
         <View style={styles.searchContainer}>
-          <Icon name="search" size={25} color="#000" />
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Search..."
-            placeholderTextColor="#000"
-            value={muscle}
-            onChangeText={setMuscle}
-            onSubmitEditing={handleSearch} 
-        />
-        <View>
+          <View style={styles.searchBar}>
+            <Icon name="search" size={25} color="#000" />
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Search for a muscle (e.g. 'bicep')"
+              placeholderTextColor="#000"
+              value={muscle}
+              onChangeText={(newText) => handleSetMuscle(newText)} // allows it so that multiple args can be passed to callback
+              // onSubmitEditing={handleSearch} 
+            />
+          </View>
+          
+          <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+            <Text style={styles.viewPlanText }>Search</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.muscleFilterDropdown}>
+            {/* <View style={styles.dropdown}>
+              <RNPickerSelect
+                  style={styles.picker}
+                  onValueChange={(value) => handleFilterChange(1, value)}
+                  items={exerciseTypes}
+                  placeholder={{label: 'Exercise', value: 'select_muscle'}}
+                  textInputProps={{color: 'white', textAlign: 'center', fontWeight: '700'}}
+                  
+              />
+            </View> */}
+
+            {/* <View style={styles.dropdown}>
+              <RNPickerSelect
+                  style={styles.picker}
+                  onValueChange={(value) => setFilteredExercises(exercises.filter((item) => {
+                   
+                    const muscleTypeMatch = !value || item.muscle === value;
+                
+                    return muscleTypeMatch;
+                  }))}
+                  items={muscleTypes}
+                  placeholder={{label: 'Muscle', value: 'select_muscle'}}
+                  textInputProps={{color: 'white', textAlign: 'center', fontWeight: '700'}}
+                  
+              />
+            </View> */}
             
+            {/* <View style={styles.dropdown}>
+              <RNPickerSelect
+                  style={styles.picker}
+                  onValueChange={(value) => setFilteredExercises(exercises.filter((item) => {
+                   
+                    const difficultyTypeMatch = !value || item.difficulty === value;
+                
+                    return difficultyTypeMatch;
+                  }))}
+                  items={difficultyTypes}
+                  placeholder={{label: 'Select Difficulty', value: 'select_muscle'}}
+                  textInputProps={{color: 'white', textAlign: 'center', fontWeight: '700'}}
+                  
+              />
+            </View> */}
         </View>
-        </View>
+
         <View style={styles.header}>
-          <Text style={styles.headerTitle}>Looking for inspiration? Type in a muscle group to see exercise examples</Text>
-          <Pressable style={styles.viewPlanButton}>
+          <Text style={styles.headerTitle}>{ closestMuscle ? 'Results shown for: ' + closestMuscle : 'Search to display results.'}</Text>
+          {/* <Pressable style={styles.viewPlanButton}>
             <Text style={styles.viewPlanText }onPress={viewPlan}>View Plan</Text>
-          </Pressable>
+          </Pressable> */}
+          <View style={styles.dropdown}>
+              <RNPickerSelect
+                  style={styles.picker}
+                  onValueChange={(value) => setFilteredExercises(exercises.filter((item) => {
+                   
+                    const difficultyTypeMatch = !value || item.difficulty === value;
+                
+                    return difficultyTypeMatch;
+                  }))}
+                  items={difficultyTypes}
+                  placeholder={{label: 'Select Difficulty', value: 'select_muscle'}}
+                  textInputProps={{color: 'white', textAlign: 'center', fontWeight: '700'}}
+                  
+              />
+            </View>
         </View>
-        <ScrollView style={styles.scrollViewContainer}>
+        <ScrollView style={styles.scrollViewContainer}
+          showsVerticalScrollIndicator={false}>
           <View style={styles.exerciseContainer}>
-            {exercises.map((exercise, index) => (
+            
+            {filteredExercises.map((exercise, index) => (
               <WorkoutItem key={index} workout={exercise} />
             ))}
           </View>
         </ScrollView>
+
+       
+        
     </View>
+    <View>
+          <Pressable style={styles.viewPlanButton}>
+            <Text style={styles.viewPlanText }onPress={viewPlan}>View Current Plan</Text>
+          </Pressable>
+        </View>
     </SafeAreaView>
   );
 };
@@ -115,7 +282,7 @@ const WorkoutItem = ({ workout }) => {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#07203A',
+    backgroundColor: '#1A2633',
   },
   container: {
     flex: 1,
@@ -149,33 +316,119 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    // width: 150,
     marginBottom: 20,
   },
   headerTitle: {
+    width: 180,
+    textAlign:'flex-start',
     fontSize: 14,
     color: '#fff',
     fontWeight: "bold",
   },
   viewPlanButton: {
     backgroundColor: '#3E89E1',
+    height: 40,
+    width: 'auto',
     paddingVertical: 8,
     paddingHorizontal: 16,
     borderRadius: 20,
+    // marginTop: 25,
+    alignSelf: 'center',
+
+  },
+  searchButton: {
+    backgroundColor: '#3E89E1',
+    height: 'auto',
+    width: 'auto',
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    // marginLeft: 45,
+    // alignSelf: 'flex-end'
   },
   viewPlanText: {
     color: '#fff',
-    fontSize: 20,
+    fontSize: 14,
     fontWeight: "bold",
     padding: 5,
   },
   searchContainer: {
+
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    // backgroundColor: "rgba(255, 255, 255, 0.5)",
     borderRadius: 10,
-    padding: 10,
-    marginVertical: 10,
-    marginBottom: 50,
+    // padding: 10,
+    // marginVertical: 10,
+    // marginBottom: 10,
+  },
+  searchBar: {
+    width: 280,
+    flexDirection: 'row',
+    // alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    // borderRadius: 10,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    padding: 9,
+    // marginVertical: 10,
+    // marginBottom: 10,
+  },
+  titleText: {
+    fontSize: 14,
+    fontWeight: '800',
+    color: 'white',
+    padding: 5,
+    marginBottom: 10,
+    // marginTop: 10,
+
+    width: 320,
+    // marginLeft: 30,
+    marginRight: 30,
+
+    alignSelf: 'flex-start',
+    // textShadowColor: 'rgba(0, 0, 0, 0.75)', // Shadow color
+    // textShadowOffset: { height: 3, width: 1 }, // Shadow offset (width, height)
+    // textShadowRadius: 6, // Shadow blur radius
+  },
+  dropdown: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 5,
+    marginHorizontal: 2,
+    // marginLeft: 5,
+    backgroundColor: '#273646',
+    borderWidth: 1,
+    borderColor: 'white',
+    // fontSize: 15,
+    // alignSelf: 'center',
+    height: 45,
+    width: 150,
+    borderColor: 'white',
+    // borderWidth: 2,
+    // padding:,
+    borderRadius: 12,
+    color: "white",
+    alignItems: 'center'
+
+},
+picker: {
+  // width: 300,
+  marginLeft: 10,
+  alignSelf: 'center',
+  color: 'white'
+},
+  dropdownText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: '700'
+},
+  muscleFilterDropdown: {
+    justifyContent: 'center',
+    marginBottom: 20,
+    flexDirection: 'row'
   },
   searchInput: {
     flex: 1,
@@ -188,9 +441,12 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     marginBottom: 30, 
   },
+  scrollViewContainer: {
+    height: 500,
+  },
   workoutItem: {
     flex: 0.8, 
-    backgroundColor: '#475E78',
+    backgroundColor: '#273646',
     padding: 20,
     borderRadius: 15,
     marginRight: 10,
@@ -207,6 +463,15 @@ const styles = StyleSheet.create({
     //backgroundColor: "red",
     flex: "row",
 
+  },
+  button: {
+    borderRadius: 20,
+    // marginHorizontal: 5,
+    marginTop: 15,
+    marginLeft: 50,
+    // width: 120,
+    padding: 10,
+    elevation: 2,
   },
   title: {
     fontSize: 24,
